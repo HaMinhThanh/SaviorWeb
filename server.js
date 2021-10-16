b0b5c1c138ac01fddd57108ddd43446677d2edd9
Thanh Ha <18521401@gm.uit.edu.vn>
2021-10-16 21:44:40 +0700

    some changes

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-//const mongoose = require("mongoose");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, 
+{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false },
+()=>{
+    console.log("Connect to MongoDB");
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // middleware
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 const PORT = process.env.PORT || 8080
 
