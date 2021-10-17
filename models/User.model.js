e8b8c45eabfdfa20e4353cb02b9aff0e37912681
Thanh Ha <18521401@gm.uit.edu.vn>
2021-10-17 11:11:14 +0700

    creat files

---
+++ models/User.model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            min: 3,
+            max: 20,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+        },
+        profilePicture: {
+            type: String,
+            default: "",
+        },
+        coverPicture: {
+            type: String,
+            default: "",
+        },
+        address: {
+            type: String,
+            default: "",
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        followings: {
+            type: Array,
+            default: [],
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        role: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema)
